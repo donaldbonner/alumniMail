4f4e3982d0ac23c99e5f550303660413d2ac9b85
The Deej <donsbons@gmail.com>
2014-03-15 14:08:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+pyMail
+------
+
+This is a server wrote in python that will process a large amount of information to send out via email.
+
+premise
+=======
+
+My fraternity has a large alumni base.  A few (okay more than a few) members still have debt.  This script will process the alumni names, emails, and debt to tailor an email message to each to politely ask for them to update their debt.
+
+Functionality
+=============
+
+This is a list of the metrics for the program.  An outline for how it will function
+
+*	Input the information from a spreadsheet(.crv) file.  Column information will go as ordered. First Name, Last Name, Email, Current Debt
+
+*	Create a generic text file to input as the email body.  There will be unique characters in each message that will be replaced on sending with first and last name, the email, and current debt owed.
+
+*	Add support for input variables.  This will allow to clear lines of alumni who have paid debt, update for partial payments, or add members to debt list.  All information will be updated in spreadsheet.
---
+++ mailEx.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Sample code from Scott Kuhls github
+https://github.com/skuhl/autograder/blob/master/ag-email.py
+"""
+
+import sys, os
+import getpass, smtplib
+
+if sys.hexversion < 0x030000F0:
+    print("This script requires Python 3")
+    sys.exit(1)
+
+emailSession = None
+
+def emailLogin(senderEmail, mypassword):
+    global emailSession
+    emailSession = smtplib.SMTP("smtp.gmail.com", 587)
+    emailSession.ehlo()
+    emailSession.starttls()
+    emailSession.ehlo
+    emailSession.login(senderEmail, mypassword)
+
+def emailLogout():
+    global emailSession
+    emailSession.quit()
+
+def sendEmail(senderEmail, recipients, subject, body):
+    headers = ["From: " + senderEmail,
+               "Subject: " + subject,
+               #"To: " + ', '.join(recipients),  
+               #Use this to send to a list of people
+               "To: " + recipients,
+               "MIME-Version: 1.0",
+               "Content-Type: text/plain"]
+    headers = "\r\n".join(headers)
+    global emailSession
+    emailSession.sendmail(senderEmail, recipients, headers + "\r\n\r\n" + body)
+
+print("Who is this sent to?")
+recipientEmail = sys.stdin.readline().strip()
+
+print("Enter the subject line for the email message.")
+subject = sys.stdin.readline().strip()
+
+print("Enter the content for the email message.")
+content = sys.stdin.readline().strip()
+
+# Login to email server
+print('Enter your username')
+senderEmail = sys.stdin.readline().strip()
+mypassword = getpass.getpass("Password for " + senderEmail + ": ")
+emailLogin(senderEmail, mypassword)
+
+sendEmail(senderEmail, recipientEmail, subject, content)
+print('Sent email to ' + recipientEmail + ' email successfully.')
+
+# logout
+emailLogout()
